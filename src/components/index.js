1f970f129da2903e0dee59bd8658bfabb29c12b8
mfranz1 <55514493+mfranz1@users.noreply.github.com>
2020-09-03 21:37:06 -0400

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+class IndexPage extends Component {
+
+    render() {
+        return (
+            <body>
+            <div>
+                <h1 id="head">Welcome to or Trello</h1>
+                hello
+            </div>
+        </body>
+        );
+    }
+}
+
+export default IndexPage;
