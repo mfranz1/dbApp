9076da898a80bc56a451e0ff573a5f3f85388715
mfranz1 <55514493+mfranz1@users.noreply.github.com>
2020-09-10 20:40:28 -0400

    Create LandingPage.js

---
+++ src/components/LandingPage.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+//import {Link} from "react-router-dom";
+console.log("beginLanding");
+class LandingPage extends Component{
+    render(){
+        return(
+            <body>
+            <div className="LandingPage">
+                <h1>Mike and Paige's Project</h1>
+            </div>
+            </body>
+        );
+    }
+}
+console.log("endLanding");
+export default LandingPage;
