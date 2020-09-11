bde27151c0797ee2417198b316147441c1b93678
mfranz1 <55514493+mfranz1@users.noreply.github.com>
2020-09-10 20:50:50 -0400

    Update LandingPage.js

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,16 +1,12 @@
 import React, {Component} from 'react';
 //import {Link} from "react-router-dom";
-console.log("beginLanding");
 class LandingPage extends Component{
     render(){
         return(
-            <body>
             <div className="LandingPage">
                 <h1>Mike and Paige's Project</h1>
             </div>
-            </body>
         );
     }
 }
-console.log("endLanding");
 export default LandingPage;
